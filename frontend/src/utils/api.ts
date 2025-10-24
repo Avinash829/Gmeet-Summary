5cc9d3f3526d58e13878936e6489c5ba0bd15111
Avinash829 <avinashpappala@gmail.com>
2025-10-24 17:04:14 +0530

    backend url updated

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
-    withCredentials: false, // not using cookies
+    baseURL: process.env.BACKEND_URL || "http://localhost:5000/api",
+    withCredentials: false,
 });
+
 
 export default api;
